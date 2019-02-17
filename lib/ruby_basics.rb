4498afa025c9d0ef83eb4e83b819d7d99f4c8946
David Thayer <dthayer16@gmail.com>
2019-02-17 21:48:13 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -2,24 +2,25 @@ def division(num1, num2)
   num1 / num2
 end
 
-def assign_variable(name)
-  name
+def assign_variable(value)
+  name = value
 end
 
-def argue (argue = 4)
-  argue
+def argue (arguement)
+  arguement
 end
 
 def greeting (value1, value2)
-  value1, value2
+  puts "#{value1}, #{value2}"
 end
 
 def return_a_value (mood = "Nice")
   mood
 end
 
-def last_evaluated_value (proficienty = "expert")
-  proficienty
+def last_evaluated_value
+  "beginner"
+  "expert"
 end
 
 def pizza_party (topping = "cheese")
